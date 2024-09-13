9fb58d9070132a783b65bbc4ea4afdbb2f74311b
Akshar <aksharpatel978@gmail.com>
2024-09-13 19:12:44 -0400

    first problem solved

---
+++ ArraysAndHashing/containsDupe.js
@@ -0,0 +1,36 @@
+// Given an integer array of nums, return true if any value appears more than once in the array, otherwise return false
+
+// Ex 1
+
+// Input: nums = [1, 2, 3, 4]
+// Output: false
+
+// Ex 2
+
+// Input: nums = [1, 2, 3, 3]
+// Output: true
+
+function dupeCheck(nums) {
+    let obj = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (obj[num] === undefined) {
+            obj[num] = 1
+        } else {
+            obj[num] ++
+        };
+
+        if (obj[num] === 2) {
+            return true
+        };
+    };
+
+    return false;
+};
+
+let arr = [1, 2, 3, 3];
+let arr2 = [1, 2, 3, 4]
+
+console.log(dupeCheck(arr));
+console.log(dupeCheck(arr2));
